3c4276c755358d5a6baa46e567ef36d674f5bc36
debfdias <debfdias@gmail.com>
2023-09-10 11:26:42 -0300

    Fix error phrase

--- interfaces/contractSchema.ts
+++ interfaces/contractSchema.ts
@@ -3,7 +3,7 @@ import z from "zod"
 export const contractSchema = z.object({
   contractNumber: z
     .string({
-      required_error: "Número de contrato é obrigatório.",
+      required_error: "Número de conta contrato é obrigatório.",
       invalid_type_error: "Número de contrato precisa ser numérico.",
     })
     .min(6, { message: "Conta contrato possui 6 dígitos." })
